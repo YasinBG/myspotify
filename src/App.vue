14e7a329bd92d32fe0e91b2923157ea15ef4ab6e
YasinBG <116117219+YasinBG@users.noreply.github.com>
2023-12-05 15:16:22 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
   <header class="flex justify-center p-4 items-center bg-gray-600 shadow-md">
     <router-link to="/">
       <h1 class="text-white text-3xl font-serif shadow-lg"><span class="text-green-400">YBG</span>Movies</h1>
-
+       <home-view></home-view>
     </router-link>
   </header>
   <main>
@@ -11,6 +11,20 @@
 </main>
 </div>
 </template>
+<script>
+import HomeView from './views/HomeView.vue';
+export default {
+  components:{
+    HomeView
+  },
+  setup(){
+
+    return {
+
+    }
+  }
+}
+</script>
 
 <style>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>test</div>
+  <div class="home "> 
+    <div class="card relative">
+
+      <router-link to="/movies">
+        <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/14e40a9c-be70-4c6a-8a94-bd4e8d71ffbb/d85l58y-11e48ee8-ea09-4684-8c34-7262da9c2a92.jpg/v1/fit/w_414,h_293,q_70,strp/naruto_by_xong_d85l58y-414w.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NjM3IiwicGF0aCI6IlwvZlwvMTRlNDBhOWMtYmU3MC00YzZhLThhOTQtYmQ0ZThkNzFmZmJiXC9kODVsNTh5LTExZTQ4ZWU4LWVhMDktNDY4NC04YzM0LTcyNjJkYTljMmE5Mi5qcGciLCJ3aWR0aCI6Ijw9OTAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.ZUBAKoMcI-H8BXLTcv559PleaMzwplevb_663giDWB8" class=" object-cover " alt="">
+
+        <div class="detail absolute p-6">
+
+          <h3>Naruto</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima sit perspiciatis vero veniam deserunt rem reprehenderit, voluptatibus asperiores voluptate, atque nisi, tempora magnam illum pariatur ducimus deleniti quaerat in corporis!</p>
+
+        </div>
+      </router-link>
+
+    </div>
+
+  </div>
 </template>
 
 <script>
