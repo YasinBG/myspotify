6003aca04552bc1c14c6282a627ad992174d68ee
YasinBG <116117219+YasinBG@users.noreply.github.com>
2023-12-06 16:17:10 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="bg-gray-600 box-border">
-  <header class="flex justify-center p-4 items-center bg-gray-600 shadow-md">
-    <router-link to="/">
-      <h1 class="text-white text-3xl font-serif shadow-lg"><span class="text-green-400">YBG</span>Movies</h1>
+  <body>
+  <div class="bg-gray-600 box-border ">
+  <header class="flex justify-center mx-3 my-4 p-4 items-center bg-gray-600 shadow-md">
+    <router-link to="/" class="">
+      <h1 class=" text-white text-3xl text-center p-6 font-serif shadow-lg"><span class="text-green-400 ">YBG</span> Movies</h1>
        <home-view></home-view>
     </router-link>
   </header>
@@ -10,6 +11,7 @@
   <router-view/>
 </main>
 </div>
+</body>
 </template>
 <script>
 import HomeView from './views/HomeView.vue';
