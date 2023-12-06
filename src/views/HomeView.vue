e5e34df6bdcf003a883a9036abf3bde9f4ed58d9
YasinBG <116117219+YasinBG@users.noreply.github.com>
2023-12-06 23:53:25 +0300

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <div class="card relative">
       <router-link to="/movie/tt3896198" class="">
         <img
-          src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/14e40a9c-be70-4c6a-8a94-bd4e8d71ffbb/d85l58y-11e48ee8-ea09-4684-8c34-7262da9c2a92.jpg/v1/fit/w_414,h_293,q_70,strp/naruto_by_xong_d85l58y-414w.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NjM3IiwicGF0aCI6IlwvZlwvMTRlNDBhOWMtYmU3MC00YzZhLThhOTQtYmQ0ZThkNzFmZmJiXC9kODVsNTh5LTExZTQ4ZWU4LWVhMDktNDY4NC04YzM0LTcyNjJkYTljMmE5Mi5qcGciLCJ3aWR0aCI6Ijw9OTAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.ZUBAKoMcI-H8BXLTcv559PleaMzwplevb_663giDWB8"
+          src="https://cdn.anisearch.com/images/character/cover/0/29_300.webp"
           alt=""
           class="block w-full h-[500px] object-cover relative z-0"
         />
@@ -27,6 +27,16 @@
       />
     </form>
     <div v-for="movie in movies" :key="movie.imdbID">
+      <div>
+        <router-link :to="'/movie/' + movie.imdbID">
+
+        <div>
+          <img :src="movie.Poster" alt="">
+          <div>{{ movie.Type }}</div>
+        </div>
+
+      </router-link>
+      </div>
       <router-link :to="'/movie/' + movie.imdbID">
 
         <div>
